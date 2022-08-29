d198f1560f4ea6fb799b37fd07fbdc6bdd0d9f9a
Lydia Wildes <lydiawildes1@gmail.com>
2022-08-29 17:00:09 -0500

    completed questions 1-3 and attempted first stretch, need more help on stretch

--- assignment/scripts/array-practice.js
+++ assignment/scripts/array-practice.js
@@ -8,11 +8,11 @@ let animalArray = ['fish', 'cat', 'bird', 'dog'];
 console.log('Animals are: ', animalArray);
 
 // 1.a. TODO: Make an array with some favorite foods
-
+let favoriteFoods = ['manapua', 'bao', 'burger', 'chicken tikka masala', 'street tacos' ];
 
 // 1.b. TODO: Log your array of foods to the console with a message, similar 
 //      to the example above
-
+console.log('My favorite foods are: ', favoriteFoods);
 
 
 // 2. Array.length - tells you how many items are in the array
@@ -22,7 +22,7 @@ console.log('--- 2. Length of an array ---');
 console.log('Number of animals:', animalArray.length);
 
 // 2.a. TODO: Log to the console the number of foods in your array
-
+console.log('Number of favorite foods:' , favoriteFoods.length);
 
 // 3. Accessing array items
 console.log('--- 3. Accessing items in an array ---');
@@ -31,14 +31,18 @@ console.log('--- 3. Accessing items in an array ---');
 console.log('First animal is', animalArray[0])
 
 // 3.a. TODO: Log the second animal in the array 
-
+console.log('Second animal is' , animalArray[1]);
 
 // 3.b. TODO: Log the last animal in the array using it's array index 
-
+//Grammar FIX-- 'it's' should be changed to 'its'
+console.log('Last animal is' , animalArray[3]);
 
 // 3.c. (STRETCH) TODO: Log the last animal by using the array length, 
 //      instead of the exact index number of the last item
+console.log('Last animal is' , animalArray.length)
+console.log (animalArray.length = 4 )
 
+//LYDIA WORK ON STRETCH ABOVE 
 
 
 // 4. Adding & Removing Array Items
@@ -49,7 +53,8 @@ animalArray.push('penguin');
 console.log('Added an animal to end,', animalArray);
 
 // 4.a. TODO: Add a new food at the end of your array & log the array
-
+favoriteFoods.push('pizza');
+console.log('Added a favorite food,', favoriteFoods);
 
 
 // Example: Remove the last animal by using Array.pop
